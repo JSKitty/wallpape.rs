f0db3b52a23be0b51a12f0a40c22be62f042af36
JSKitty <jskitty@protonmail.com>
2022-06-15 01:56:23 +0100

    [Backend] Add file extension guard/filter
    This will prevent crashes of the frontend which were caused by non-image files being piped into image sources.
    
    Now the Rust backend will filter files by a hardcoded file extension, it's not perfect, but in most cases it gets the job done!

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -2,15 +2,28 @@ use tauri::{command};
 use std::fs;
 use std::process;
 
+fn is_file_supported(str_dir: String) -> bool {
+    // TODO: Optimise, surely a better way to do this? Would be nice to have `supported_exts` as a global static somehow too.
+    let supported_exts: Vec<&str> = vec!["jpg", "jpeg", "png"];
+    let mut spliced_ext: Vec<&str> = str_dir.split(".").collect();
+    if spliced_ext.is_empty() {
+        return false;
+    }
+    let str_ext = spliced_ext.pop().unwrap();
+    return supported_exts.contains(&str_ext);
+}
+
 #[command]
 pub fn fetch_wallpaper_paths(str_dir: String) -> Result<Vec<String>, String> {
     // Fetch all file paths that exist in the wallpaper directory
     let paths = fs::read_dir(str_dir).unwrap();
     // Map each file path into a String vector
     let dir_vec: Vec<String> = paths.map(|path| path.unwrap().path().display().to_string()).collect();
+    // Filter into supported extensions only
+    let validated_dir_vec: Vec<String> = dir_vec.iter().filter(|dir| is_file_supported(dir.to_string())).cloned().collect();
     // Send it off to the renderer!
-    (!dir_vec.is_empty())
-        .then(|| dir_vec)
+    (!validated_dir_vec.is_empty())
+        .then(|| validated_dir_vec)
         .ok_or("Yikes!".to_string())
 }
 
